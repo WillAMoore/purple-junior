cb8005afb0c830b270a4215367a0c8658cf363a6
Will <william@purplegroup.com>
2015-04-27 13:07:00 +0100

    things

--- template-home.php
+++ template-home.php
@@ -3,14 +3,14 @@
 <?php get_header(); ?>
 
 <!-- template-home.php -->
-
-<div class="section-1 .container-fluid">
+<div class="container-fluid">
+<div class="section-1 .row">
 
 	<p class="col-xs-6 col-xs-offset-3"> We Are a Digital Studio With Different Backgrounds But Only One Goal</p>
 
 </div>
 
-<div class="section-2 .container-fluid">
+<div class="section-2 .row">
 
 	<div class="col-sm-4 col-xs-12">
 		<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img1.png"></img>
@@ -25,21 +25,21 @@
 
 </div>
 
-<div class="section-3 .container-fluid">
+<div class="section-3 .row">
 
 </div>
 
-<div class="section-4 .container-fluid">
+<div class="section-4 .row">
 
 </div>
 
-<div class="section-5 .container-fluid">
+<div class="section-5 .row">
 
 </div>
 
-<div class="section-6 .container-fluid">
+<div class="section-6 .row">
 
 </div>
-
+</div>
 
 <?php get_footer(); ?>
