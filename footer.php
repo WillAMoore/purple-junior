4e9b15eb81432985c3d8230aaad4d1eb8f465449
Will <william@purplegroup.com>
2015-04-27 13:57:58 +0100

    very basic layout

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <!-- footer.php -->
 
 <div class="footer container">
-
+	<div class="col-xs-12">
+		<div class="col-xs-12 text-center">
+			<i class="fa fa-heart col-xs-6 col-xs-offset-3 text-center"></i>
+		</div>
+	</div>
 </div>
 
 
--- template-home.php
+++ template-home.php
@@ -3,43 +3,69 @@
 <?php get_header(); ?>
 
 <!-- template-home.php -->
-<div class="container-fluid">
-<div class="section-1 .row">
 
-	<p class="col-xs-6 col-xs-offset-3"> We Are a Digital Studio With Different Backgrounds But Only One Goal</p>
+	<div class="home-section section-1  col-xs-12">
 
-</div>
+		<p class="col-xs-6 col-xs-offset-3 text-center"> We Are a Digital Studio With Different Backgrounds But Only One Goal</p>
 
-<div class="section-2 .row">
-
-	<div class="col-sm-4 col-xs-12">
-		<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img1.png"></img>
-	</div>
-	<div class="col-sm-4 col-xs-12">
-		<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img2.png"></img>
-	</div>
-	<div class="col-sm-4 col-xs-12">
-		<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img3.png"></img>
 	</div>
 
+	<div class="home-section section-2  col-xs-12">
 
-</div>
+		<div class="col-sm-4 col-xs-12 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img1.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-12 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img2.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-12 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img3.png"></img>
+		</div>
 
-<div class="section-3 .row">
 
-</div>
+	</div>
 
-<div class="section-4 .row">
+	<div class="home-section section-3  col-xs-12">
+		<div class="col-xs-12">
+			<h2 class="col-xs-6 col-xs-offset-3 text-center">Showcase</h2>
+			<p class="col-xs-6 col-xs-offset-3 text-center">  Here will be the grid, made using the essential grid plugin </p>
+		</div>
 
-</div>
+	</div>
 
-<div class="section-5 .row">
+	<div class="home-section section-4  col-xs-12">
+		<div class="col-xs-12 text-center">
+			<h2 class="col-xs-6 col-xs-offset-3 text-center">Web Marketing</h2>
+			<i class="fa fa-heart col-xs-6 col-xs-offset-3 text-center"></i>
+			<img src="<?php echo get_template_directory_uri();?>/img/home-sect4-fullgraphic.png"></img>
+		</div>
 
-</div>
+	</div>
 
-<div class="section-6 .row">
+	<div class="home-section section-5  col-xs-12">
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/cisco-grey.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/calix-grey.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/swarovski.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/silkstream-grey.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/theshipleopard-grey.png"></img>
+		</div>
+		<div class="col-sm-4 col-xs-6 text-center">
+			<img src="<?php echo get_template_directory_uri();?>/img/cadenza-grey.png"></img>
+		</div>
+	</div>
 
-</div>
-</div>
+	<div class="home-section section-6  col-xs-12">
+		<h2 class="col-xs-6 col-xs-offset-3 text-center">Tell us how you feel today</h2>
+		<p class="col-xs-6 col-xs-offset-3 text-center">  Here will be the form, made using the contact form 7 plugin </p>
+	</div>
 
 <?php get_footer(); ?>
