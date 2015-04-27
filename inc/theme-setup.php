e37105763bdad9d7365fa214552584c4d726ab1f
Will <william@purplegroup.com>
2015-04-27 11:30:47 +0100

    more setup

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -2,9 +2,9 @@
 
 // Load scripts and stylesheets required by the theme.
 function iw_enqueue_scripts() {
+    
     wp_enqueue_script('jquery');
     wp_enqueue_script('bs-script', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js');
-    wp_enqueue_script('iw-script', get_template_directory_uri() . '/js/main.js', array('jquery', 'bs-script'), '1.0', true);
 
     wp_enqueue_style('bs-style', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_enqueue_style('fa-style', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
