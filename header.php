83787610acccedc709352c0ca8f532b9cbbef153
Will <william@purplegroup.com>
2015-05-01 10:09:38 +0100

    layout for search

--- header.php
+++ header.php
@@ -11,27 +11,28 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div >
-    <nav id="navbar" class="navbar navbar-default">
-        <div class="col-sm-6 col-xs-12">
-            <img src="<?php echo get_template_directory_uri();?>/img/logo-purple-junior.png"></img>
-        </div>
-        <div class="col-sm-6 hidden-xs">
-            <p id="seacrhicon"  class="circle"> </p>
-          <div class="pull-right">
-              <!--          <img src="<?php echo get_template_directory_uri();?>/img/icon-search.png"></img>
-                <input type="text">
-                <img src="<?php echo get_template_directory_uri();?>/img/icon-share.png"></img>
-            -->
-
-                <div class="group">
-                    <input type="text" required>
-                    <span class="highlight"></span>
-                    <span class="bar"></span>
-                    <label> <em> Search for... </em></label>
-                </div>
-            <p  id="shareicon" class="circle"></p>
-
+    <div >
+        <nav id="navbar" class="navbar navbar-default">
+            <div class="col-sm-6 col-xs-12">
+                <img src="<?php echo get_template_directory_uri();?>/img/logo-purple-junior.png"></img>
             </div>
-        </div>
-    </nav> <!-- /#navbar -->
+            <div class="col-sm-6 hidden-xs">
+
+                <div class="pull-right header-space">
+                    <div class="col-xs-1">
+                        <p id="seacrhicon"  class="circle"> </p>
+                    </div>
+                    <div class="col-xs-7">
+                        <div class="group">
+                            <input type="text" required>
+                            <span class="highlight"></span>
+                            <span class="bar"></span>
+                            <label> <em> Search for... </em></label>
+                        </div>
+                    </div>
+                    <div class="col-xs-1">
+                        <p  id="shareicon" class="circle"></p>
+                    </div>
+                </div>
+            </div>
+        </nav> <!-- /#navbar -->
