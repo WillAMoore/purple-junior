cde2dbb7666f75e5c79af1f48d991930c49eaaea
Will <william@purplegroup.com>
2015-05-01 15:43:42 +0100

    menu work

--- header.php
+++ header.php
@@ -10,25 +10,51 @@
     <?php wp_head(); ?>
 </head>
 
+<?php wp_enqueue_script('show-menu-js', get_template_directory_uri() . '/js/show-menu.js',array('jquery')); ?>
+
 <body <?php body_class(); ?>>
     <div >
         <nav id="navbar" class="navbar navbar-default">
             <div class="col-sm-6 col-xs-12">
-                <img src="<?php echo get_template_directory_uri();?>/img/logo-purple-junior.png"></img>
+                <div id="menu" class="left">
+                    <div class="logo-sidemenu"> Purple Junior</div>
+                    <ul>
+                        <li> <a href="#"> Home</a></li>
+                        <li> <a href="#"> Showcase </a>
+                        <ul>
+                            <li><a href="#">ui / ux web & apps </a></li>
+                            <li><a href="#">web application</a></li>
+                            <li><a href="#">custom relation management</a></li>
+                            <li><a href="#">location based application</a></li>
+                            <li><a href="#">branding</a></li>
+                            <li><a href="#">printing</a></li>
+                        </ul>
+                        </li>
+                        <li> <a href="#">Digital Marketing </a> </li>
+                        </li>
+                        <li><a href="#">Contact Us</li>
+                    </ul>
+                    <div class="column">
+                    <a href="#" id="showmenu">
+                        <p id="menuicon" class="menu-btn"> </p>
+                    </a>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-6 hidden-xs">
 
                 <div class="pull-right header-space">
-                    <div class="col-xs-1">
-                        <p id="seacrhicon"  class="circle"> </p>
-                    </div>
-                    <div class="col-xs-7">
+
+                    <div class="col-xs-6">
                         <div class="group">
                             <input type="text" required>
                             <span class="highlight"></span>
                             <span class="bar"></span>
                             <label> <em> Search for... </em></label>
                         </div>
+                    </div>
+                    <div class="col-xs-2">
+                        <p id="seacrhicon"  class="circle"> </p>
                     </div>
                     <div class="col-xs-1">
                         <p  id="shareicon" class="circle"></p>
--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -5,7 +5,10 @@ function pj_enqueue_scripts() {
 
     wp_enqueue_script('jquery');
     wp_enqueue_script('bs-script', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js');
+    wp_enqueue_script('mmenu-js', get_template_directory_uri() . '/js/jquery.mmenu.min.all.js');
+    wp_enqueue_script('jq-js', 'https://code.jquery.com/jquery-1.11.3.min.js');
 
+    wp_enqueue_style('jq-style', get_template_directory_uri() . '/css/jquery.mmenu.all.css');
     wp_enqueue_style('bs-style', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_enqueue_style('fa-style', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
     wp_enqueue_style('lato-font', 'http://fonts.googleapis.com/css?family=Lato:300,400&subset=latin,latin-ext');
