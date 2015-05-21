46a3c914cfb2687157f57bd22b414a8953d14022
Will <william@purplegroup.com>
2015-05-21 09:34:27 +0100

    footer content

--- footer.php
+++ footer.php
@@ -2,15 +2,55 @@
 
 <div class="footer">
 	<div class="footer-section-1 col-xs-12 text-center">
-		<i class="fa fa-heart col-xs-6 col-xs-offset-3 text-center"></i>
+		<div class="col-sm-3 col-xs-12 text-left">
+			<h3>About Us<h3>
+			<p>
+				From design to development Purple Junior is writing
+				something SEO correct and have only three lines max
+				or up to the point that everything looks in harmony.
+			</p>
+		</div>
+		<div class="col-sm-3 col-xs-12 text-left">
+			<h3>News Feeds<h3>
+			<p>
+				Do not give up on adding YOUR very own personal style
+				to your #weddingdaylook http://t.co/2HJvay2rbW
+				http://t.co/N3vA9YgRce, 24 hours ago
+			</p>
+		</div>
+		<div class="col-sm-3 col-xs-12 text-left">
+			<h3>Our Services<h3>
+			<p>
+				Our UI/UX web &amp; app design, branding, printing,
+				custom relational management system, location based
+				applications, applications for events.
+			</p>
+		</div>
+		<div class="col-sm-3 col-xs-12 text-left">
+			<h3>Purple Junior<h3>
+			<p>
+				<p>
+					Unit 21 &amp; 22 Big Yellow<br>
+					8/9 Rodney Road<br>
+					Southsea <br>
+					PO4 8BF
+				</p>
+
+				<p>020 7193 7997</p>
+
+				<p><a href="mailto:sayhello@purplejunior.com">sayhello@purplejunior.com</a></p>
+			</p>
+		</div>
+
 	</div>
 
 	<div class="footer-section-2 col-xs-12">
+		<i class="fa fa-heart col-xs-6 col-xs-offset-3 text-center"></i>
 		<div class="col-sm-4 col-xs-12 text-left">
 			<p>Purple Junior all rights reserved 2015</p>
 		</div>
 		<div class="col-sm-4 col-xs-12 text-center">
-			<p>sayhello@purplejunior.com</p>
+			<p><a href="mailto:sayhello@purplejunior.com">sayhello@purplejunior.com</a></p>
 		</div>
 		<div class="col-sm-4 col-xs-12 text-right">
 			<p>...</p>
