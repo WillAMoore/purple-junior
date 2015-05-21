b52f9d5f295744702f86acd5b44be7bf73d37a04
Will <william@purplegroup.com>
2015-05-21 09:14:58 +0100

    changed footer

--- footer.php
+++ footer.php
@@ -1,9 +1,23 @@
 <!-- footer.php -->
 
 <div class="footer">
-	<div class="col-xs-12 text-center">
+	<div class="footer-section-1 col-xs-12 text-center">
 		<i class="fa fa-heart col-xs-6 col-xs-offset-3 text-center"></i>
 	</div>
+
+	<div class="footer-section-2 col-xs-12">
+		<div class="col-sm-4 col-xs-12 text-left">
+			<p>Purple Junior all rights reserved 2015</p>
+		</div>
+		<div class="col-sm-4 col-xs-12 text-center">
+			<p>sayhello@purplejunior.com</p>
+		</div>
+		<div class="col-sm-4 col-xs-12 text-right">
+			<p>...</p>
+		</div>
+	</div>
+
+
 </div>
 
 
