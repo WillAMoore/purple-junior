f44a20e2193977a3a1f1c1f7d157c4cd764fea23
Dylan Scott <dylanrhysscott@gmail.com>
2015-06-01 14:46:37 +0100

    Mobile First Almost Done

--- template-home.php
+++ template-home.php
@@ -10,10 +10,10 @@
 	</div>
 
 	<div class="home-section home-section-2 row">
+		<div class="button-container">
+			<button type="button" class="btn btn-default">Our Story</button>
+		</div>
 			<div class='col-xs-12 text-center'>
-				<div class="button-container">
-					<button type="button" class="btn btn-default">Our Story</button>
-				</div>
 					<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img1.png"></img>
 					<h3>Design</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -41,7 +41,7 @@
 			<h2 class="text-center">Web Marketing</h2>
 			<span class="fa fa-heart text-center"></span>
 				<img class='img-responsive' src="<?php echo get_template_directory_uri();?>/img/4-img1.png">
-				<img class="dotted-img" src="<?php echo get_template_directory_uri();?>/img/4-dotted-line.png">
+				<img class="hidden-xs dotted-img" src="<?php echo get_template_directory_uri();?>/img/4-dotted-line.png">
 						<div class="digital-promo">
 							<h3 class="small-caps">Digital Promotion</h3>
 							<p>
@@ -51,75 +51,60 @@
 						</div>
 
 				<div>
-					<img class='mobile-line' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
+					<img class='visible-xs mobile-line' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
 				</div>
 
 
-
-
-
-				<div>
-					<div>
-						<div class="social-strategy">
-							<div>
-								<h3 class="small-caps">Social Media Strategy</h3>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
-								</p>
-							</div>
-							<div>
-								<img class="bee-pic" src="<?php echo get_template_directory_uri();?>/img/4-img2.png">
-								<div>
-									<img class='mobile-line' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
-								</div>
-							</div>
-
-						</div>
+				<div class="social-strategy row">
+					<div class='col-xs-12'>
+						<h3 class="small-caps">Social Media Strategy</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
+						</p>
 					</div>
-				</div>
-
-
-
-
-
-
-
-				<div>
-					<div class="landing-pages">
-						<div class="landing-inner">
-							<h3 class="small-caps">Landing Pages</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
-							</p>
-						</div>
+					<div class='col-xs-12'>
+						<img class="bee-pic" src="<?php echo get_template_directory_uri();?>/img/4-img2.png">
 						<div>
-							<div>
-								<div>
-									<img class='frog' src="<?php echo get_template_directory_uri();?>/img/4-img3.png">
-								</div>
-								<div>
-									<img class='keyboard' src="<?php echo get_template_directory_uri();?>/img/4-img4.png">
-								</div>
-							</div>
+							<img class='visible-xs mobile-line' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
 						</div>
-
-
 					</div>
 				</div>
+
+
+
+
+
+
+
+				<div class="row">
+					<div class="col-xs-12">
+						<h3 class="small-caps">Landing Pages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
+						</p>
+					</div>
 				</div>
 
+				<div class='row'>
+					<div class='col-xs-6'>
+						<img class='frog' src="<?php echo get_template_directory_uri();?>/img/4-img3.png">
+					</div>
+					<div class='col-xs-6'>
+						<img class='keyboard' src="<?php echo get_template_directory_uri();?>/img/4-img4.png">
+					</div>
+				</div>
 
-
-			<div class="col-xs-12 landing-pages">
-				<div>
-					<h3 class="small-caps">SEO &amp; Analytics Magement</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
-					</p>
-					<img class='graph' src="<?php echo get_template_directory_uri();?>/img/graph.png" alt="" />
+				<div class='row'>
+					<div class="col-xs-12">
+						<h3 class="small-caps">SEO &amp; Analytics Magement</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
+						</p>
+						<img class='graph' src="<?php echo get_template_directory_uri();?>/img/graph.png" alt="" />
+					</div>
 				</div>
 
 			</div>
@@ -151,7 +136,7 @@
 		<div class='col-xs-12'>
 			<?php echo do_shortcode('[contact-form-7 id="9" title="Contact form 1"]'); ?>
 		</div>
-		<div>
+		<div class='col-xs-12'>
 				<a href="http://www.google.com">
 					<p class="circle social-button facebook-color">
 						<i class="fa fa-facebook"></i>
