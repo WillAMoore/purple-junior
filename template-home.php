31644bc3acf400ada6232a0227b22a7252f67276
Dylan Scott <dylanrhysscott@gmail.com>
2015-06-01 09:51:42 +0100

    Added line image

--- template-home.php
+++ template-home.php
@@ -11,22 +11,22 @@
 
 	</div>
 
-	<div class="home-section home-section-2  col-xs-12">
-		<div class="col-xs-12 button-container">
+	<div class="home-section home-section-2  col-sm-12">
+		<div class="col-sm-12 button-container">
 			<button type="button" class="btn btn-default">Our Story</button>
 		</div>
-		<div class="col-xs-12">
-			<div class="col-md-4 col-xs-12 text-center">
+		<div class="col-sm-12">
+			<div class="col-md-4 col-sm-12 text-center">
 				<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img1.png"></img>
 				<h3>Design</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
-			<div class="col-md-4 col-xs-12 text-center">
+			<div class="col-md-4 col-sm-12 text-center">
 				<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img2.png"></img>
 				<h3>Web Apps</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
-			<div class="col-md-4 col-xs-12 text-center">
+			<div class="col-md-4 col-sm-12 text-center">
 				<img src="<?php echo get_template_directory_uri();?>/img/home-sect2-img3.png"></img>
 				<h3>Web Marketing</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -35,26 +35,26 @@
 
 	</div>
 
-	<div class="home-section home-section-3 col-xs-12">
-		<div class="col-xs-12">
-			<h2 class="col-xs-6 col-xs-offset-3 text-center">Showcase</h2>
-			<div class="col-xs-10 col-xs-offset-1">
+	<div class="home-section home-section-3 col-sm-12">
+		<div class="col-sm-12">
+			<h2 class="col-sm-6 col-sm-offset-3 text-center">Showcase</h2>
+			<div class="col-sm-10 col-sm-offset-1">
 				<?php echo do_shortcode('[ess_grid alias="showcase-grid"]'); ?>
 			</div>
 		</div>
 
 	</div>
 
-	<div class="home-section home-section-4 col-xs-12">
+	<div class="home-section home-section-4 col-sm-12">
 		<div class='row'>
 			<h2 class="text-center">Web Marketing</h2>
-			<span class="col-xs-12 fa fa-heart text-center"></span>
+			<span class="col-sm-12 fa fa-heart text-center"></span>
 				<img class='img-responsive' src="<?php echo get_template_directory_uri();?>/img/4-img1.png">
-				<img class="img-responsive dotted-img" src="<?php echo get_template_directory_uri();?>/img/4-dotted-line.png">
+				<img class="hidden-xs img-responsive dotted-img" src="<?php echo get_template_directory_uri();?>/img/4-dotted-line.png">
 
-				<div class="col-xs-12">
+				<div class="col-sm-12">
 					<div class="row">
-						<div class="digital-promo col-xs-7 pull-right">
+						<div class="digital-promo col-xs-8 col-xs-offset-2 col-sm-7">
 							<h3 class="small-caps">Digital Promotion</h3>
 							<p>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -64,24 +64,31 @@
 					</div>
 				</div>
 
+				<div class="row visible-xs">
+					<img class='mobile-line visible-xs' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
+				</div>
 
 
 
 
 
-				<div class="col-xs-12">
+				<div class="col-sm-12">
 					<div class="row">
 						<div class="social-strategy">
-							<div class='col-xs-offset-2 col-xs-6'>
+							<div class='col-xs-8 col-xs-offset-2 col-sm-offset-2 col-sm-6'>
 								<h3 class="small-caps">Social Media Strategy</h3>
 								<p>
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 									Curabitur volutpat orci eget dolor ultrices auctor. Morbi eget dolor nec erat rhoncus ornare eu vitae risus.
 								</p>
 							</div>
-							<div class="col-xs-3">
+							<div class="col-xs-8 col-xs-offset-2  col-sm-3">
 								<img class="img-responsive bee-pic" src="<?php echo get_template_directory_uri();?>/img/4-img2.png">
+								<div class="visible-xs">
+									<img class='mobile-line visible-xs' src="<?php echo get_template_directory_uri();?>/img/4-mobile-line.png" />
+								</div>
 							</div>
+
 						</div>
 					</div>
 				</div>
